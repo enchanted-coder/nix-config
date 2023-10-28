66897744f3794bd30b67f59481859158e165bb11
enchanted-coder <danieltogey@pm.me>
2023-10-28 18:35:35 +0300

    added nix gc and nix autoupdate and steam to configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -45,10 +45,6 @@
   # Set your time zone.
   time.timeZone = "Africa/Nairobi";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
@@ -66,6 +62,12 @@
    };
  };
   
+  # Enable steam
+  #programs.steam = {
+  #  enable = true;
+  #  remotePlay.openFirewall = true;
+  #  dedicatedServer.openFirewall = true;
+  #};
 
   # Enable hyprland
   programs.hyprland = {
@@ -84,9 +86,10 @@
   };
   
   hardware = {
-    opengl.enable = true;
+    # opengl.enable = true;
     bluetooth.enable = true;
   }; 
+
   services.blueman.enable = true;
   services.gnome.gnome-keyring.enable = true;
 
@@ -109,9 +112,11 @@
     jack.enable = true; 
   };  
 
+  # Enable virtualisation
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
   
+ # Thunar File Manager 
   programs.thunar.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
    thunar-archive-plugin
@@ -128,12 +133,14 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nx0enjoyer = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "libvirtd" "networkmanager" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       # packages go here
+      (opera.override { proprietaryCodecs = true; })
     ];
   };
-  
+ 
+  # Enable fonts
   fonts = {
     fontconfig = {
       enable = true;
@@ -143,7 +150,8 @@
     };
     fonts = with pkgs; [ (nerdfonts.override { fonts = [ "Meslo" "JetBrainsMono" ]; }) ];
   };
-  
+ 
+  # Allow unfree and insecure packages
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowInsecure = true;
   nixpkgs.config.PermittedInsecurePackages = [
@@ -165,13 +173,6 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # List services that you want to enable:
    
@@ -181,20 +182,18 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  # Garbage collector
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
+  system.autoUpgrade = {
+    enable = true;
+    channel = "https://nixos.org/channels/nixos-23.05";
+  };
+
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
